14cd6c3b4051c1938a0bcf36eaedde4143c3b7c5
tameez4254 <tameezshaik516@gmail.com>
2026-05-27 09:21:42 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,18 @@
+async function sendQuestion(){
 
+    let question = document.getElementById("question").value;
+
+    let response = await fetch("/ask",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            question:question
+        })
+    });
+
+    let data = await response.json();
+
+    document.getElementById("response").innerHTML = data.answer;
+}
